9ea50075d765f565aeffb3e97047f11d7c68ac5a
Phil Kipkemboi <Kipkemboiphil@gmail.com>
2019-08-29 17:32:08 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const apiKey = '3d56a8e9862246eb8127bf7e9ed61b38'
+const defaultSource = 'the-washington-post';
+const sourceSelector = document.querySelector('#sources');
+const newsArticles = document.querySelector('main');
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () =>
+      navigator.serviceWorker.register('sw.js')
+        .then(registration => console.log('Service Worker registered'))
+        .catch(err => 'SW registration failed'));
+  }
+
+window.addEventListener('load', e => {
+    sourceSelector.addEventListener('change', evt => updateNews(evt.target.value));
+    updateNewsSources().then(() => {
+      sourceSelector.value = defaultSource;
+      updateNews();
+    });
+  });
+  
+  window.addEventListener('online', () => updateNews(sourceSelector.value));
+  
+  async function updateNewsSources() {
+    const response = await fetch(`https://newsapi.org/v2/sources?apiKey=${apiKey}`);
+    const json = await response.json();
+    sourceSelector.innerHTML =
+      json.sources
+        .map(source => `<option value="${source.id}">${source.name}</option>`)
+        .join('\n');
+  }
+  
+  async function updateNews(source = defaultSource) {
+    newsArticles.innerHTML = '';
+    const response = await fetch(`https://newsapi.org/v2/top-headlines?sources=${source}&sortBy=top&apiKey=${apiKey}`);
+    const json = await response.json();
+    newsArticles.innerHTML =
+      json.articles.map(createArticle).join('\n');
+  }
+
+function createArticle(article){
+    return `
+    <div class="article">
+        <a href="${article.url}">
+            <h2>${article.title}</h2>
+            <img src="${article.urlToImage}">
+            <p>${article.description}</p>
+        </a>
+    </div>
+    `;
+}
+
+//3d56a8e9862246eb8127bf7e9ed61b38 
